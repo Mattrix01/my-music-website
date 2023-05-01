bf557158e82152ae10e35015c0b32e2a6c9fb67f
Mattrix01 <emjay.knight@gmail.com>
2023-05-01 13:05:14 +0100

    site animation and dropdown fix

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// navabr toggle
+/*==================== toggle icon navbar ====================*/
 let menuIcon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
@@ -7,9 +7,9 @@ menuIcon.onclick = () => {
   navbar.classList.toggle("active");
 };
 
-// scroll sections active
+/*==================== scroll sections active link ====================*/
 let sections = document.querySelectorAll("section");
-let navLinks = document.querySelectorAll("header navbar a");
+let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
   sections.forEach((sec) => {
@@ -22,17 +22,17 @@ window.onscroll = () => {
       navLinks.forEach((links) => {
         links.classList.remove("active");
         document
-          .querySelector("header navbar a[href*=" + id + "]")
+          .querySelector("header nav a[href*=" + id + "]")
           .classList.add("active");
       });
     }
   });
-  // sticky navbar
+  /*==================== sticky navbar ====================*/
   let header = document.querySelector("header");
 
   header.classList.toggle("sticky", window.scrollY > 100);
 
-  //remove toggle icon when click over nav bar
+  /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
   menuIcon.classList.remove("bx-x");
   navbar.classList.remove("active");
 };
